60884d607bfa283fdb2dfa16b19a8e1a1508bc97
BenSh1 <47560422+BenSh1@users.noreply.github.com>
2023-12-13 20:59:01 +0200

    Add files via upload

---
+++ snake_game/checkFil.py
@@ -0,0 +1,13 @@
+
+
+
+# with open("my_file.txt", mode="r") as file:
+#     contents = file.read()
+#     print(contents)
+
+
+# with open("new_file.txt", mode="w") as file:
+#     file.write("\nsadasdasdsad")
+    
+with open("my_file.txt", mode="a") as file:
+    file.write("\n 213123123")
