fce9a10467b37546030c23d0dd05b4e5481217ef
BenSh1 <47560422+BenSh1@users.noreply.github.com>
2025-04-29 08:19:48 +0300

    Add files via upload

---
+++ NATO-alphabet-start/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+    word = input('Enter your word:').upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        print(output_list)
